146ab29c8acdca88c2de658ad33bc161574c8587
tamicaa-mat <tamicaa.mat@gmail.com>
2025-04-14 22:12:59 -0300

    clase persona

--- src/ejercicio1/Persona.java
+++ src/ejercicio1/Persona.java
@@ -1,5 +1,51 @@
 package ejercicio1;
 
 public class Persona {
+	private String apellido;
+	private String nombre;
+	private String dni; 
+	
+	
+	public Persona() {
+		
+		
+	}
+	
+	public Persona(String apellido,String nombre,String dni) {
+		this.apellido=apellido;
+		this.nombre=nombre;
+		this.dni=dni;
+		//
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	
+	public String toString() {
+		return "Persona [apellido=" + apellido + ", nombre=" + nombre + ", dni=" + dni + "]";
+	}
 
 }
