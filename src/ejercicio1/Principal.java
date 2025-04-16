c3b5ba9b7ed8b332c324fb3d377bdd7a396f6b72
diazemilianoariel <emiliano.diaz@alumnos.frgp.utn.edu.ar>
2025-04-16 05:48:43 -0300

    correccion en la ruta para leer el archivo Personas.txt

--- src/ejercicio1/Principal.java
+++ src/ejercicio1/Principal.java
@@ -1,13 +1,38 @@
 package ejercicio1;
 
+
+import java.util.ArrayList;
+import java.util.List;
 public class Principal {
 
 	public static void main(String[] args) {
 		
 		// a partir de aca se estaría leyendo el archivo Personas.txt
 		
+		/*Leer el archivo llamado Personas.txt, que está compuesto por Nombre
+		– Apellido – Dni y pasar los datos que se encuentran en el archivo a una
+		lista que no acepte datos duplicados, que permita ordenar los datos
+		según el Apellido desde la A – Z*/
+		
+		
+		Archivo archivo = new Archivo();
+		
+		archivo.setRuta("src/Personas.txt");
+		
+		
+		archivo.lee_letra_x_letra();
+		
+		List<Persona> listaPersonas = new ArrayList<>();
+		
+		
+		
+		
+		
+		
 	
 
 	}
+
+	
 
 }
